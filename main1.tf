20d5a03d2a4ea2cdcb00aa407cc85b73d1dcf8cc
Cloud User <azureuser@VM-b-01.suh2ytfng44enj552emmzunquh.ux.internal.cloudapp.net>
2024-11-16 14:26:30 +0000

    commit main tf for 1st scenario

---
+++ main1.tf
@@ -0,0 +1,48 @@
+provider "azurerm" {  # declaring the cloud provider
+  features {}
+  subscription_id = "61f12577-4a11-4912-9c83-bfeee540b1f7"  # Optional if you have 'az login' configured
+}
+ 
+resource "azurerm_resource_group" "rg01" {  # creating a new resource group
+  location = "East US"
+  name     = "rg01"
+  tags = {
+    env   = "dev"
+    dep   = "finance"
+    owner = "Anisha"
+    proj1 = "p1"
+  }
+}
+ 
+resource "azurerm_virtual_network" "vnet01" {  # defining the virtual network
+  location            = azurerm_resource_group.rg01.location
+  resource_group_name = azurerm_resource_group.rg01.name
+  name                = "Vnet01"
+  # Multiple address spaces can be defined here
+  address_space = [
+    "10.10.0.0/16",
+    "10.20.1.0/24",  # Additional address space
+    "10.20.2.0/24"   # Additional address space
+  ]
+  tags = {
+    env   = "dev"
+    dep   = "finance"
+    owner = "Anisha"
+    proj1 = "p1"
+  }
+}
+# Subnet 1
+resource "azurerm_subnet" "subnet01" {
+  name                 = "subnet01"
+  resource_group_name  = azurerm_resource_group.rg01.name
+  virtual_network_name = azurerm_virtual_network.vnet01.name
+  address_prefixes     = ["10.20.1.0/25"]  # First subnet, using part of 10.20.1.0/24
+}
+ 
+# Subnet 2
+resource "azurerm_subnet" "subnet02" {
+  name                 = "subnet02"
+  resource_group_name  = azurerm_resource_group.rg01.name
+  virtual_network_name = azurerm_virtual_network.vnet01.name
+  address_prefixes     = ["10.20.1.128/25"]  # Second subnet, using the remaining part of 10.20.1.0/24
+}
